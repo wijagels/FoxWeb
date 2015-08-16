44417710b7dc09d39d0f985389a5503ba6c7c0f4
William Jagels <will.jagels@gmail.com>
2015-08-16 03:42:44 -0700

    Remove some needless trash

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var api = require('./routes/api');
 
 var app = express();
@@ -24,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'html')));
 
 app.use('/', api);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
